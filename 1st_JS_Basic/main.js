62c3dcf636ff7cf6346e5db95fd9a070e0e07716
Basheerahmed34 <154429694+Basheerahmed34@users.noreply.github.com>
2023-12-20 13:16:23 +0000

    function

--- 1st_JS_Basic/main.js
+++ 1st_JS_Basic/main.js
@@ -1,2 +1,6 @@
 console.log("New Repo on the Github");
 console.log("Basheer  is a clever boy");
+function  Promise((resolve, reject)=>{
+    reject(45);
+    console.log("Script is also reject And We give Your Amount Return ")
+})
